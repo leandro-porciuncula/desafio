d7d7901f9e05a4be265d8318896fab82f571fee5
leandro-porciuncula <64909980+leandro-porciuncula@users.noreply.github.com>
2025-11-27 21:54:20 -0300

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.status(200).send('Desafio AWS');
+  res.status(200).send('Desafio AWS, UpperStack');
 });
 
 // Servir imagens da pasta uploads
